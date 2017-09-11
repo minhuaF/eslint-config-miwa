187a2ef07605c69f7c0910103a380a1885a2c38c
minhuaF <905102996@qq.com>
2017-09-11 22:32:10 +0800

    test

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # eslint-config-miwa
 
 > eslint init
+
+- 全局安装 eslint
+`npm install -g eslint `
+
+- 关联eslint到本地项目
+` npm link eslint-config-miwa `
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 module.exports = {
+  // 设置解析器选项
+  parser: 'babel-eslint',
+  parserOptions: {
+    'ecmaVersion' : 6,       // 设置想使用的ECMAScript版本，默认3,4
+    'sourceType': 'module',  // 默认值script，若代码是ECMAScript模块的话，使用的是“module
+    'ecmaFeatures' : {       // 设置额外的语言特征
+      'jsx' : true
+    }
+  },
   extends: [
     'standard',
     'standard-react'
   ],
+  //脚本运行的环境
   env: {
-    node: true,
-    es6: true
+    'browser': true,
+    'node': true,
+    'es6': true
   },
+  // 设置全局变量
+  globals: {},
+  // 使用第三方插件
+  plugins: {},
+  // 启用的规则以及各自的错误级别
   rules: {
-    'no-console' : 'off',
-    'indent' : ['error', 2],
-    'quotes': ['error', 'single']
+    // 禁止在条件语句中出现赋值语句，除非用圆括号括起来
+    'no-cond-assign': 'except-parens', 
+    // 允许使用console
+    'no-console': ''
+    
   }
 }
